530ebda3b68648e90a891575b60a56dcc65ba3da
Geraldo Luiz <geraldobl58@gmail.com>
2024-12-30 17:12:03 -0300

    feat: add common DTOs, decorators, and guards; implement JWT authentication guard and update service integration

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -7,9 +7,9 @@ import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 import * as Joi from 'joi';
-import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -21,14 +21,15 @@ import { JwtStrategy } from './strategies/jwt.strategy';
         MONGODB_URI: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRATION: Joi.string().required(),
-        PORT: Joi.number().required(),
+        HTTP_PORT: Joi.number().required(),
+        TCP_PORT: Joi.number().required(),
       }),
     }),
     JwtModule.registerAsync({
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
-          expiresIn: `${configService.get<string>('JWT_EXPIRATION')}`,
+          expiresIn: `${configService.get<string>('JWT_EXPIRATION')}s`,
         },
       }),
       inject: [ConfigService],

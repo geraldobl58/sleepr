13c263ba9d98ad774ed875873bfd7f35e1fef0c6
Geraldo Luiz <geraldobl58@gmail.com>
2025-01-04 15:55:50 -0300

    feat: add notifications package with nodemailer dependency; update users repository to import Logger correctly

--- apps/auth/src/users/users.repository.ts
+++ apps/auth/src/users/users.repository.ts
@@ -1,7 +1,9 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AbstractRepository, UserDocument } from '@app/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+
+import { Logger } from '../../../../node_modules/@nestjs/common';
 
 @Injectable()
 export class UsersRepository extends AbstractRepository<UserDocument> {
